9437b11e690a519cb4c752981834c65c1cee0aae
Kowalski7cc <kowalski.7cc@xspacesoft.com>
2016-10-18 14:01:07 +0200

    Optimize localization class

--- src/com/xspacesoft/kowalski7cc/rtmmodding/Messages.java
+++ src/com/xspacesoft/kowalski7cc/rtmmodding/Messages.java
@@ -15,21 +15,19 @@ along with PS3 Customization.  If not, see <http://www.gnu.org/licenses/>.*/
 
 package com.xspacesoft.kowalski7cc.rtmmodding;
 
-import java.util.Arrays;
-import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
 	
-	private final static String[] SUPPORTED_LANGUAGES = new String[] {
-		"it",
-		"hu",
-		"de",
-		"fr",
-	};
+//	private final static String[] SUPPORTED_LANGUAGES = new String[] {
+//		"it",
+//		"hu",
+//		"de",
+//		"fr",
+//	};
 	
-	private static final String BUNDLE_NAME = "com.xspacesoft.kowalski7cc.rtmmodding.locales.messages" + getLocale(); //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "com.xspacesoft.kowalski7cc.rtmmodding.locales.messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
 			.getBundle(BUNDLE_NAME);
@@ -44,11 +42,11 @@ public class Messages {
 		}
 	}
 
-	private static String getLocale() {
-		if(Arrays.asList(SUPPORTED_LANGUAGES).contains(Locale.getDefault().getLanguage()))
-			return "_" + Locale.getDefault().getLanguage();
-		return "";
-	}
+//	private static String getLocale() {
+//		if(Arrays.asList(SUPPORTED_LANGUAGES).contains(Locale.getDefault().getLanguage()))
+//			return "_" + Locale.getDefault().getLanguage();
+//		return "";
+//	}
 
 
 //	public static boolean isForceEnglish() {
